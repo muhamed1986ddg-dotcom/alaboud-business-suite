5ead14a7c268353c8d4aca8c3804b632f6e033bb
Muhamed <muhamed1986ddg@gmail.com>
2026-07-20 15:14:19 -0400

    v18.7.7 add customer old balance to general debt

--- frontend/src/version.js
+++ frontend/src/version.js
@@ -1,2 +1,2 @@
-export const APP_VERSION="v18.7.6 Tawasul Balance + Version Fix";
-export const APP_VERSION_NUMBER="18.7.6";
+export const APP_VERSION="v18.7.7 Customer Old Balance Debt Fix";
+export const APP_VERSION_NUMBER="18.7.7";
